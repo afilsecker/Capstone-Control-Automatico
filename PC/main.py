393ceb64cb16d0755a898eed9df1ef85cb91a8a0
Alex <afilsecker@uc.cl>
2021-11-09 23:09:01 -0300

    intentos varios

--- PC/main.py
+++ PC/main.py
@@ -1,6 +1,18 @@
 """This is the main program"""
 
-from frontend import Window
+import sys
 
-window = Window()
-window.mainloop()
+from PyQt5.QtWidgets import QApplication
+
+from frontend.interfaz import Interfaz
+
+from backend.client import Client
+
+# Para poder debbuguear
+def hook(type_error, traceback):
+    print(type_error)
+    print(traceback)
+
+
+if __name__ == '__main__':
+    Client()
