c789473e6415733a2c75fce56c420a7066f33e0c
Alex <afilsecker@uc.cl>
2021-12-08 11:14:44 -0300

    lañskdjf

--- raspberry/motores.py
+++ raspberry/motores.py
@@ -5,7 +5,7 @@ from multiprocessing.connection import Connection
 from multiprocessing import Pipe, Lock, Process
 from threading import Thread
 from time import sleep, time
-import numpy as np
+import random
 
 
 class Motores:
@@ -24,8 +24,12 @@ class Motores:
     def generar_diccionario_acciones(self):
         self.action_dict = {
             'set_vels': self.set_vels,
-            'reset': self.reset
-        }
+            'send_calibrate': self.send_calibrate,
+            'reset': self.reset,
+            'send_sleep': self.send_sleep,
+            'send_awake': self.send_awake,
+            'send_center': self.send_center
+            }
 
     def handle_capstone(self):
         while True:
@@ -56,8 +60,8 @@ class Motores:
     def listen(self):
         while not self.closed:
             if self.serial.in_waiting > 0:
-                line = self.serial.readline().decode('UTF-8')
-                print(line[:-1])
+                c = self.serial.read()
+                print(f"recibido: {c}")
 
     def send_capstone(self, envio):
         with self.lock:
@@ -76,8 +80,27 @@ class Motores:
         self.serial.write(vel_A_bit)
         vel_B_bit = int.to_bytes(abs(vel_B), 1, 'big')
         self.serial.write(vel_B_bit)
-        print('send_time = {:<6.2f} ms'.format(
-            (time() - self.start_time) * 1000))
+        # print('send_time = {:<6.2f} ms'.format((time() - self.start_time) * 1000))
+
+    def send_calibrate(self):
+        bit = pow(2, 6)
+        bit = int.to_bytes(bit, 1, 'big')
+        self.serial.write(bit)
+
+    def send_sleep(self):
+        bit = pow(2, 5)
+        bit = int.to_bytes(bit, 1, 'big')
+        self.serial.write(bit)
+
+    def send_awake(self):
+        bit = pow(2, 4)
+        bit = int.to_bytes(bit, 1, 'big')
+        self.serial.write(bit)
+
+    def send_center(self):
+        bit = pow(2, 3)
+        bit = int.to_bytes(bit, 1, 'big')
+        self.serial.write(bit)
 
 
 def motores_process(motores_pipe, lock_send):
@@ -93,11 +116,55 @@ if __name__ == '__main__':
     vel_B = -255
     sleep(2)
     while True:
-        conn2.send(['set_vels', {'vel_A': vel_A, 'vel_B': vel_B}])
-        vel_A += 1
-        if vel_A > 255:
-            vel_A = -255
-        vel_B += 1
-        if vel_B > 255:
-            vel_B = -255
-        sleep(0.1)
+        try:
+            a = int(input('1 para calibrar, 2 para velocidad random: '))
+            if a == 1:
+                conn2.send(['send_calibrate', None])
+                print("calibrate")
+            elif a == 2:
+                vel_A = random.randint(-255, 255)
+                vel_B = random.randint(-255, 255)
+                print(f"vel_A = {vel_A}, vel_B = {vel_B}")
+                conn2.send(['set_vels', {'vel_A': vel_A, 'vel_B': vel_B}])
+            elif a == 0:
+                vel_A = 0
+                vel_B = 0
+                print(f"vel_A = {vel_A}, vel_B = {vel_B}")
+                conn2.send(['set_vels', {'vel_A': vel_A, 'vel_B': vel_B}])
+            elif a == 3:
+                vel_A = int(input("vel_A: "))
+                vel_B = 0
+                print(f"vel_A = {vel_A}, vel_B = {vel_B}")
+                conn2.send(['set_vels', {'vel_A': vel_A, 'vel_B': vel_B}])
+            elif a == 4:
+                vel_A = 0
+                vel_B = 0
+                mult = 2
+                for i in range(100):
+                    vel_A = i * mult
+                    vel_B = i * - mult
+                    mult = - mult
+                    print(f"vel_A = {vel_A}, vel_B = {vel_B}")
+                    conn2.send(['set_vels', {'vel_A': vel_A, 'vel_B': vel_B}])
+                    sleep(0.1)
+            elif a == 5:
+                for _ in range(100):
+                    vel_A = random.randint(-255, 255)
+                    vel_B = random.randint(-255, 255)
+                    print(f"vel_A = {vel_A}, vel_B = {vel_B}")
+                    conn2.send(['set_vels', {'vel_A': vel_A, 'vel_B': vel_B}])
+                    sleep(0.1)
+            elif a == 6:
+                conn2.send(['send_sleep', None])
+            elif a == 7:
+                conn2.send(['send_awake', None])
+            elif a == 8:
+                conn2.send(['send_center', None])
+            elif a == 9:
+                print("custom vels:")
+                vel_A = int(input('vel_A: '))
+                vel_B = int(input('vel_B: '))
+                conn2.send(['set_vels', {'vel_A': vel_A, 'vel_B': vel_B}])
+
+        except Exception:
+            pass
